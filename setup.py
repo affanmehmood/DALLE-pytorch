9274c5b7e92f2c34eef6f363ecbba87b4f89a67d
Affan Mehmood <46571643+affanmehmood@users.noreply.github.com>
2022-09-27 11:49:04 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     'rotary-embedding-torch',
     # 'taming-transformers-rom1504',
     'tokenizers',
-    'torch==1.8.1',
+    'torch>=1.9',
     'torchvision',
     'transformers',
     'tqdm',
